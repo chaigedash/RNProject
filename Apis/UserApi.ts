bf3afb6d3ad0395d37b50184857efbfd096d34c9
pLittleChay <111098086+chaigedash@users.noreply.github.com>
2024-12-16 12:18:56 +0800

    登陆注册、首页和用户页的前端实现

--- Apis/UserApi.ts
+++ Apis/UserApi.ts
@@ -7,6 +7,17 @@ export const getUser = (id: string) => {
     return new Promise((resolve, reject) => {
         api.get(`/user/getUser?id=${id}`).then(res => {
             resolve(res.data)
+        }).catch(err => {
+            console.log(err);
         })
     });
+}
+export const updateUser = (params: any) => {
+    return new Promise((resolve, reject) => {
+        api.post('/user/updateUser', params).then(res => {
+            resolve(res.data)
+        }).catch(err => {
+            console.log(err);
+        })
+    })
 }
--- Apis/testApi.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-    baseURL: 'http://10.0.2.2:8080',
-});
-export const getTestApi = () => {
-    return api.get('/test/testapi');
-}
